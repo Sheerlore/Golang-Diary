875434ca74cc1db6d714d00fd4c742e47c352e57
uai <noema01@outlook.jp>
2020-07-28 04:46:27 +0900

    add Goticker file

--- GoTickers/main.go
+++ GoTickers/main.go
@@ -7,18 +7,24 @@ import (
 
 func backgroundTask() {
 	ticker := time.NewTicker(1 * time.Second)
-
 	for _ = range ticker.C {
-		fmt.Println("tock")
+		fmt.Println("Tock")
 	}
 }
 
 func main() {
-	fmt.Println("Go ticker")
+	fmt.Println("Go Tickers Tutorial")
 
 	go backgroundTask()
 
+	//This print statement will be executed before
+	//the first 'tock' prints in the console
 	fmt.Println("The rest of my application can continue")
 
-	select {}
+	//here we use an empty select{} in order to keep
+	// our main function alive indefinitely as it would
+	// complete before our backgroundTask has a chance
+	// to execute if we didn't.
+
+	select{}
 }
