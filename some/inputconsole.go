d914be63dc40e2a4b3b2519bbe4f7b92a65f6d79
uai <noema01@outlook.jp>
2020-07-28 06:39:12 +0900

    add inputconsole.go

--- some/inputconsole.go
+++ some/inputconsole.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	//"strings"
+)
+
 func main() {
-	
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Println("Simple shell")
+	//fmt.Println("---------------------------")
+
+	//for {
+	//	fmt.Println("->")
+	//	text, _ := reader.ReadString('\n')
+
+	//	//convert CRLF to LF
+	//	text = strings.Replace(text, "\n" , "", -1)
+
+	//	if strings.Compare("hi", text) == 0 {
+	//		fmt.Println("Hello Yourself")
+	//	}
+	//}
+	reader := bufio.NewReader(os.Stdin)
+	char, _, err := reader.ReadRune()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(char)
+
+	switch char {
+	case 'A':
+		fmt.Println("A key pressed")
+		break
+	case 'a':
+		fmt.Println("a key pressed")
+		break
+	}
 }
